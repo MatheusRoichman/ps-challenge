c9c64532bf7a4af54cbce6c8cb34349f9ced7e77
Matheus Lima Roichman <matheus.lima@biomainvestimentos.com>
2023-08-18 23:05:18 -0300

    chore: allow Yelp images on Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // allow media1, media2, media3, media4 from s3-media*.fl.yelpcdn.com, where * is a wildcard
+        hostname: "*.fl.yelpcdn.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
